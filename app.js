5617c376c6f6b9261f4107d9df2f358b5eed64fa
rangerbd <dipto.g1@gmail.com>
2021-12-06 21:16:42 +0600

    final commit

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 fetch('https://restcountries.com/v3.1/all')
 .then(res => res.json())
 .then(data => {
-    console.log(data);
+    
     const countries=document.getElementById('countri');
 
     for (let i = 0; i < data.length; i++) {
         const country = data[i];
-        console.log(country.name.common)
+        
         const div=document.createElement('div');
         div.className='country';
 
         const countryInfo = `
             <h3 class="country-name"> ${country.name.common} </h3>
             <p> ${country.capital}</p>
-            <button> click me</button>
+            <button onclick="displaycountryDetail('${country.name.common}')"> details</button>
         `;
 
        div.innerHTML=countryInfo;
         countries.appendChild(div);
         
     }
+
 })
+
+const displaycountryDetail = common =>{
+    const url = 'https://restcountries.com/v3.1/name/'+common;
+    fetch(url)
+   .then(res => res.json())
+   .then(data => countryDetail(data[0]))
+    
+   const countryDetail = country =>{
+        console.log(country)
+       const countryDiv=document.getElementById('countryDiv');
+       countryDiv.innerHTML= `
+         <h1> ${country.population} </h1>
+       <h1> ${country.name.official} </h1>
+      <h1> ${country.name.common} </h1>
+      <img src="${country.flags.png}"> </img>
+
+       `
+   }
+}
